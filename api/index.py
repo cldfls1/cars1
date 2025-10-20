4abf2281b6e44104ae72d9f33915843acb7f3633
cldfls1 <denchirovv@gmail.com>
2025-10-20 15:17:45 +0300

    Remove Mangum - use FastAPI ASGI directly

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 """
 Vercel serverless function entry point
 """
-from mangum import Mangum
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
